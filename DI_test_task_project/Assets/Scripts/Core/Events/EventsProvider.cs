019b44f0e42782acc0bc3d56ff7628b921138766
YurkinaSonya <60286792+YurkinaSonya@users.noreply.github.com>
2025-12-07 22:49:29 +0700

    Events base
    Implemented side-based component for event management. It is required for cross-system communication and better injection

---
+++ DI_test_task_project/Assets/Scripts/Core/Events/EventsProvider.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Core.Events
+{
+    /// <summary>
+    /// Specific container of event-like classes for EventAggregator.
+    /// There defines this classes as basical containers of data, 
+    /// transferring cross parts of system by events.
+    /// </summary>
+    public static class EventsProvider
+    {
+        
+    }
+}
